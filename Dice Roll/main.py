0e147abaf05abae93b5d93dbbf49438c15e17356
Utkarsha-Satpute <83292109+Utkarsha-Satpute@users.noreply.github.com>
2021-10-25 19:45:50 +0530

    Update main.py

--- Dice Roll/main.py
+++ Dice Roll/main.py
@@ -1,6 +1,6 @@
 import random
 while True:
-     user_input = input("do you want to roll the dice?")
+     user_input = input("Do you want to roll the dice?")
      a = random.randint(1, 9)
      if user_input == 'yes':
          if a > 6:
@@ -9,4 +9,4 @@ while True:
              print(a)
      elif user_input == 'no':
          break
-print("thanks for using\ncode by utkarsha")
+print("thanks for using,code by utkarsha")
